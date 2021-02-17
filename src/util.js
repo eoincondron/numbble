c5b25672db80f4ab6fb1499a9a546bee328a4df4
unknown <econdr@gmail.com>
2021-02-17 18:44:45 +0000

    Add util for spliting a multiple digit num string into a list of single digits separated by spaces

--- src/util.js
+++ src/util.js
@@ -9,3 +9,18 @@ export let NUMBERS = '0123456789'.split('');
 export function random_digit(max) {
     return Math.floor(Math.random() * max);
 }
+
+
+export function split_num_string(num_string) {
+    // Splits a string into a list of individual characters separated by spaces.
+    // Example: split_num_string('123') -. ['1', ' ', '2', ' ', '3']
+    let chars = num_string.split('')
+    if (chars.length === 0 || !NUMBERS.includes(chars[0])) {
+        throw "Cannot split num string: " + num_string;
+    }
+    let n_chars = chars.length;
+    for (let i = 0; i < n_chars - 1; i++) {
+        chars.splice(2*i + 1, 0, SPACE)
+    }
+    return chars
+}
