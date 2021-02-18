bbe4c09584183191c7a78267c85d0020dc3aebc6
unknown <econdr@gmail.com>
2021-02-18 21:30:56 +0000

    Rename TileArray.array to tile_array

--- src/tile_array.js
+++ src/tile_array.js
@@ -43,41 +43,41 @@ export class TileArray {
         // If brackets are in the way we do nothing as this is valid state.
         // If space is not empty we throw as the method should not be called in this state
         // If the space has other non-numeric characters adjacent we throw as this is invalid state.
-        let content = this.array[space_location];
+        let content = this.tile_array[space_location];
         if (content !== SPACE) {
             throw "Cannot join numbers, space contains " + content;
         }
-        let to_left = this.array[space_location - 1];
+        let to_left = this.tile_array[space_location - 1];
         if (to_left === R_BRACKET) {
             return
             // Maybe we want to raise here and catch downstream. Decide Later.
             // We may also want to allow it if there is a left bracket to the right.
         }
-        let to_right = this.array[space_location + 1];
+        let to_right = this.tile_array[space_location + 1];
         if (to_right === L_BRACKET) {
             return
         }
         if (!(NUMBERS.includes(to_left) && NUMBERS.includes(to_right))) {
             throw "Space is adjacent non-numeric characters :".concat(to_left, ', ', to_right)
         }
-        this.array[space_location - 1] = to_left + to_right; // numbers are represented as strings
-        this.array.splice(space_location, 2);
+        this.tile_array[space_location - 1] = to_left + to_right; // numbers are represented as strings
+        this.tile_array.splice(space_location, 2);
     }
 
     split_numbers(location) {
         // Replace multi-digit number at location with a sub-list of individual digits and spaces.
         // Used to reverse calls to join_numbers
-        let content = this.array[location];
+        let content = this.tile_array[location];
         let split = split_num_string(content);
-        this.array.splice(location, 1, ...split)
+        this.tile_array.splice(location, 1, ...split)
     }
 
     insert_operation(space_location, operation) {
         // Insert an operation into an empty space.
         // Throw if array does not contain a SPACE at space_location.
-        let content = this.array[space_location];
+        let content = this.tile_array[space_location];
         if (content === SPACE) {
-            this.array[space_location] = operation;
+            this.tile_array[space_location] = operation;
         } else {
             throw "Cannot insert operation, space contains " + content;
         }
@@ -86,16 +86,16 @@ export class TileArray {
     remove_operation(op_location) {
         // Remove an operation from the array, replacing with a SPACE.
         // Throw if array does not contain an operation at the given location.
-        let content = this.array[op_location];
+        let content = this.tile_array[op_location];
         if (OPERATIONS.includes(content)) {
-            this.array[op_location] = SPACE;
+            this.tile_array[op_location] = SPACE;
         } else {
             throw "Cannot remove operation, space contains " + content;
         }
     }
 
     _check_contains_num_at(num_location, action) {
-        let content = this.array[num_location];
+        let content = this.tile_array[num_location];
         let msg = "Cannot " + action;
         if (!is_num_string(content)) {
             throw msg.concat(" as there is no number at ", num_location, ". Content: ", content)
@@ -106,14 +106,14 @@ export class TileArray {
         // Insert a bracket to the left of the chosen number
         // Throw if array does not contain a number at the given location
         this._check_contains_num_at(num_location, 'insert a bracket')
-        this.array.splice(num_location, 0, L_BRACKET)
+        this.tile_array.splice(num_location, 0, L_BRACKET)
     }
 
     insert_right_bracket(num_location) {
         // Insert a bracket to the right of the chosen number
         // Throw if array does not contain a number at the given location
         this._check_contains_num_at(num_location, 'insert a bracket')
-        this.array.splice(num_location + 1, 0, R_BRACKET)
+        this.tile_array.splice(num_location + 1, 0, R_BRACKET)
     }
 
     remove_brackets(num_location) {
@@ -121,13 +121,13 @@ export class TileArray {
         // Throw if the array does not contain a number here
         this._check_contains_num_at(num_location, "remove brackets")
         let i = num_location - 1;
-        while (this.array[i] === L_BRACKET) {
-            this.array.splice(i, 1)
+        while (this.tile_array[i] === L_BRACKET) {
+            this.tile_array.splice(i, 1)
             i -= 1;
         }
         i = num_location + 1;
-        while (this.array[i] === R_BRACKET) {
-            this.array.splice(i, 1)
+        while (this.tile_array[i] === R_BRACKET) {
+            this.tile_array.splice(i, 1)
             i += 1;
         }
     }
@@ -137,14 +137,14 @@ export class TileArray {
         // Do nothing if number is already negative
         // Throw if a number is not contained at the given location
         this._check_contains_num_at(num_location, "negate number")
-        let num_string = this.array[num_location];
+        let num_string = this.tile_array[num_location];
         if (!num_string.startsWith('-')) {
-            this.array[num_location] = '-' + num_string
+            this.tile_array[num_location] = '-' + num_string
         }
     }
 
     _build_display_string() {
-        return this.array.join('')
+        return this.tile_array.join('')
     }
 
     _get_sub_lists() {
@@ -154,8 +154,8 @@ export class TileArray {
         let sub_lists = [];
         let current_list = [];
 
-        for (i = 0; i < this.array.length; i++) {
-            content = this.array[i];
+        for (i = 0; i < this.tile_array.length; i++) {
+            content = this.tile_array[i];
             if (content === SPACE) {
                 sub_lists.push(current_list);
                 current_list = [];
